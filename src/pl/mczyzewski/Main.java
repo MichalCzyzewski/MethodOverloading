56f16580f3dbd0420c2c77ff083912fd4ec73d87
Michal Czyzewski <czy.mich@gmail.com>
2019-04-23 13:35:06 +0200

    add method calculateScore

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -1,8 +1,18 @@
 package pl.mczyzewski;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+
+        System.out.println(calculateScore("Michał", 234));
+
+    }
+
+    public static int calculateScore(String playername, int score)
+    {
+        System.out.println("Player " + playername + " score " + score + " score points ");
+        return score * 1000;
     }
 }
