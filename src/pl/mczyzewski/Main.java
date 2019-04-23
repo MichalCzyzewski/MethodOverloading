7ac21eef69ccc5cb4709b7f25745d850f2a9dc35
Michal Czyzewski <czy.mich@gmail.com>
2019-04-23 13:36:49 +0200

    use calculateScore and println

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -6,7 +6,8 @@ public class Main
     public static void main(String[] args)
     {
 
-        System.out.println(calculateScore("Michał", 234));
+        calculateScore("Michał", 200);
+
 
     }
 
